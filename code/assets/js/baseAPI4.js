9a99b11c4e02ae9ed2c23cea306f80099e307dc6
joey <2744238120@qq.com>
2020-08-01 22:50:41 +0800

    完成了

--- code/assets/js/baseAPI4.js
+++ code/assets/js/baseAPI4.js
@@ -2,6 +2,31 @@
 $.ajaxPrefilter(function(options){
 
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+    // console.log(options.url);
+    // 为需要请求头的设置统一的方式
+    if(options.url.indexOf('/my/') !== -1) {
 
-    console.log(options.url);
+        options.headers = {
+
+            Authorization:localStorage.getItem('token' || '')
+            }
+    }
+
+    // 全局统一挂载complete函数
+     options.complete = 
+        function (res) {
+            console.log(res);
+
+            if (res.responseJSON.status !== 0 &&
+                res.responseJSON.message !== "获取用户基本信息成功！") {
+
+                // 清空本地存储的token
+                localStorage.removeItem('token')
+                // 页面跳转回登录页面
+
+                location.href = '/code/login4.html'
+
+            }
+        
+     }
 })
